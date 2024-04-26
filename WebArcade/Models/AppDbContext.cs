772b2fd04be09631b810d2711fccf83ea923ec2e
NathanLuizz <nathan.aquino@sga.pucminas.br>
2024-04-26 14:21:24 -0300

    configBD

---
+++ WebArcade/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebArcade.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Games> Games { get; set; }
+
+
+    }
+}
---
+++ WebArcade/Models/Games.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebArcade.Models
+{
+    [Table("Games")]
+    public class Games
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Obrigatorio informar o nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar a desenvolvedora")]
+        public string Desenvolvedora { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar o ano do lançamento")]
+        public int Anolancamento { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar o genero")]
+        public string Genero { get; set; }
+    }
+}
