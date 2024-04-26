e194f114560663aace6e8936862235e498ed176d
NathanLuizz <nathan.aquino@sga.pucminas.br>
2024-04-26 11:48:22 -0300

    Adicionar arquivos de projeto.

---
+++ WebArcade/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebArcade.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
